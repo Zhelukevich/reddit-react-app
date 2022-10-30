1bfff7bdaafa9c3ffc2bd44026aff8f4f0fa711e
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2022-10-30 18:30:26 +0300

    add helmet

--- src/server/server.js
+++ src/server/server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
-// app.use(helmet({
-// 	contentSecurityPolicy: false,
-// }));
+app.use(helmet({
+	contentSecurityPolicy: false,
+}));
 
 app.use('/static', express.static('./dist/client'));
 

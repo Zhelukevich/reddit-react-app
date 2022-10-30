f1a2b09fc1e38111ddce17a7542a534745107b2a
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2022-10-30 14:10:54 +0300

    fix_error

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -15,7 +15,7 @@ export function UserBlock({ avaterSrc, username, loading }: IUserBlockProps) {
 
 	return (
 		<a
-			href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity`}
+			href={`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=https://skillbox-reddit-react-app.herokuapp.com/auth&duration=permanent&scope=read submit identity`}
 			className={styles.userBox}
 		>
 
